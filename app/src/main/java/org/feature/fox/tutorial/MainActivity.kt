f5c3e622c0a9ba384b4adde185bd22e17732cf05
Julian Holfeld <julian@uni-kassel.de>
2022-05-01 10:41:02 +0200

    feat: add snackbar

--- app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
+++ app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
@@ -1,19 +1,25 @@
 package org.feature.fox.tutorial
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage : ImageView
+    private lateinit var diceImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton: Button = findViewById(R.id.roll_button)
+        val fab: View = findViewById(R.id.fab)
         diceImage = findViewById(R.id.dice_image)
+
         rollButton.setOnClickListener { rollDice() }
+        fab.setOnClickListener { view -> fabSnack(view) }
     }
 
     private fun rollDice() {
@@ -26,5 +32,10 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableResource)
+    }
+
+    private fun fabSnack(view: View) {
+        Snackbar.make(view, "This FAB needs an action!", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show()
     }
 }
