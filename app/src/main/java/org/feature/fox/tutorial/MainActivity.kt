3ba62d2ef376f93eb0009601977236a87c2b29f4
Julian Holfeld <julian@uni-kassel.de>
2022-05-01 10:05:46 +0200

    feat: add dice resources

--- app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
+++ app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
@@ -2,7 +2,7 @@ package org.feature.fox.tutorial
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +14,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when ((1..6).random()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
