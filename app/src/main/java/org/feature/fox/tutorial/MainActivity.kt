55cfb01be5e76232a6c455b398fc5856bb73321d
Julian Holfeld <julian@uni-kassel.de>
2022-05-01 10:16:46 +0200

    refactor: use lateinit

--- app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
+++ app/src/main/java/org/feature/fox/tutorial/MainActivity.kt
@@ -6,15 +6,17 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
+        diceImage = findViewById(R.id.dice_image)
         rollButton.setOnClickListener { rollDice() }
     }
 
     private fun rollDice() {
-        val diceImage: ImageView = findViewById(R.id.dice_image)
         val drawableResource = when ((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
